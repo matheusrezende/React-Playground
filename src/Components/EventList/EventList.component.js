732eb895b93c0629d142de9598d6a37dd79034e5
Matheus Rezende <matheus.rezende10@gmail.com>
2018-12-16 20:07:11 +0700

    style pagination buttons

--- src/Components/EventList/EventList.component.js
+++ src/Components/EventList/EventList.component.js
@@ -9,19 +9,20 @@ export default ({events, page, changePage}) => (
     {events.map((item, index) => (
       <EventItem key={index} event={item} />
     ))}
-    <div>
+    <div className='event-list__pagination'>
       {
         // dont show in case the user is in the first page
         page > 1 && (
-          <button type='button' onClick={() => changePage(page - 1)}>
+          <button className='event-list__pagination__button' type='button' onClick={() => changePage(page - 1)}>
             {'<< '}
         Previous
           </button>
         )}
+      <span>|</span>
       {
         // the default page size is 10, so if theres less then ten items it means we are on the last page
         events.length === 10 && (
-          <button type='button' onClick={() => changePage(page + 1)}>
+          <button className='event-list__pagination__button' type='button' onClick={() => changePage(page + 1)}>
           Next
             {' >>'}
           </button>
